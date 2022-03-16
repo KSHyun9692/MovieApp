e25cbda4b983c52e3680ebedb176394b575d3b54
KSHyun9692 <KSHyun9692@gmail.com>
2022-03-16 21:28:23 +0900

    MovieApp

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '3fc6d255ce4b03673d35d36a5cb86e76';
+export const IMG_BASE_URL = 'https://image.tmdb.org/t/p/';
--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_KEY, API_URL, IMG_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null);
+
+    useEffect(() => {
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetch(endPoint)
+        .then(response => response.json())
+        .then(response => {
+
+            setMovies([response.results]);
+            setMainMovieImage(response.results[0]);
+        })
+        
+    
+    }, []);
+
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width:'100%', margin: '0'}}>
+
+            {/* Main Image */}
+            {MainMovieImage &&
+            <MainImage image={`${IMG_BASE_URL}w1280${MainMovieImage.backdrop_path}`} />
+            }
+            <div style={{ width:'85%', margin: '1rem auto'}}>
+            
+                <h2>Movie by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards */}
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+
+        </div>
     )
 }
 
